be01c94c3fafcb213f63194855dbe9265e23eb41
Wrixxit <113486239+Wrixxit@users.noreply.github.com>
2025-04-23 12:02:56 -0500

    updated batch files

--- run_fetch.bat
+++ run_fetch.bat
@@ -2,9 +2,18 @@
 cd /d "%~dp0"
 call venv\\Scripts\\activate
 
-echo "Fetching table data..."
+echo [SCRIPT] Fetching table data...
 python fetch_tables.py
+
+echo [SCRIPT] Extracting projects...
 python projects.py --file projects.csv --projects
+
+echo [SCRIPT] Assigning judges to projects...
 python projects.py --file projects.csv --assign
+
+echo [SCRIPT] Extracting contacts list...
 python projects.py --file projects.csv --contacts . --export .
+
+echo [SCRIPT] All processes completed successfully...
+
 pause
--- run_scores.bat
+++ run_scores.bat
@@ -2,10 +2,20 @@
 cd /d "%~dp0"
 call venv\\Scripts\\activate
 
-echo "Updating table data..."
+echo [SCRIPT] Updating table data...
 python fetch_tables.py
+
+echo [SCRIPT] Extracting overall team scores...
 python scores.py --file scores.csv --overall --export overall_scores.txt
+
+echo [SCRIPT] Extracting all track scores...
 python scores.py --file scores.csv --exportall
+
+echo [SCRIPT] Extracting all track scores...
 python scores.py --file scores.csv --list --export all
+
+echo [SCRIPT] Checking for potential cheating teams...
 python scores.py --file scores.csv --cheat --export cheat_check.txt
+
+echo [SCRIPT] All processes completed successfully...
 pause
